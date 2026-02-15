6e3b1f6d9016a00733284fbf03ff4a754b389d6c
shanga <shashankrao997@gmail.com>
2026-02-15 11:07:23 +0530

    Add files via upload

---
+++ Firmware/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/* SK6812 RGB */
+#define RGBLIGHT_DEFAULT_HUE 170
+#define RGBLIGHT_SLEEP
+#define WS2812_DI_PIN GP3                       
+
+/* OLED (SSD1306 via I2C) */
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP6
+#define I2C1_SCL_PIN GP7
+#define OLED_BRIGHTNESS 190
+
+/* Debounce */
+#define DEBOUNCE 5
+
+/* NKRO */
+#define FORCE_NKRO
+
